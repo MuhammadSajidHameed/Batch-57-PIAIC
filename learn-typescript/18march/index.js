3d97a2005d79132dad77a85a0a5534ddc1a7dee6
Muhammad Sajid Hameed <sa6027835@gmail.com>
2024-04-02 20:03:25 +0500

    initial commit

---
+++ learn-typescript/18march/index.js
@@ -0,0 +1,38 @@
+"use strict";
+// Declare a variable for storing your favorite movie name with a string type annotation.
+let favoritemovie = "TheMessage";
+console.log(favoritemovie);
+// Create a template string to introduce yourself, including your name and age.
+let myname = "MuhammadSajidHameed";
+let age = 25;
+let introduce = `My name is ${myname} and i am ${age} years old.`;
+console.log(introduce);
+//Initialize a constant with a type annotation for your current mood as a boolean.
+const calmmood = true;
+console.log(calmmood);
+//Combine a user's first and last name using template literals and log the full name.
+let firstname = "Muhammad Sajid";
+let secondname = "Hameed";
+let fullname = `${firstname} ${secondname}`;
+console.log(fullname);
+//Create a variable for a user's age with a type annotation and assign a number to it.
+let presentage = 25;
+console.log(presentage);
+//Use a template string to display a product name and its price.
+let productname = "Coke";
+let productprice = 200;
+let productdetails = `Product name ${productname}, & price ${productprice}`;
+console.log(productdetails);
+// Declare a variable to store the number of pets you have with a number type annotation.
+let numberofpets = 4;
+console.log(numberofpets);
+//Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let todayisbirthday = false;
+console.log(todayisbirthday);
+//Create a template string that includes a user's first name, last name, and age with proper labels.
+let firstnam = "Muhammad Sajid";
+let secondnam = "Hameed";
+let ages = 25;
+let mydetails = `My name is ${firstnam} ${secondnam} and i am ${ages} years old.`;
+console.log(mydetails);
+/* My name is Sajid */
---
+++ learn-typescript/18march/index.ts
@@ -0,0 +1,44 @@
+// Declare a variable for storing your favorite movie name with a string type annotation.
+let favoritemovie: string = "TheMessage";
+console.log(favoritemovie);
+
+// Create a template string to introduce yourself, including your name and age.
+let myname: string = "MuhammadSajidHameed";
+let age: number = 25;
+let introduce: string = `My name is ${myname} and i am ${age} years old.`;
+console.log(introduce);
+
+//Initialize a constant with a type annotation for your current mood as a boolean.
+const calmmood: boolean = true;
+console.log(calmmood);
+
+//Combine a user's first and last name using template literals and log the full name.
+let firstname: string = "Muhammad Sajid";
+let secondname: string = "Hameed";
+let fullname: string = `${firstname} ${secondname}`;
+console.log(fullname);
+
+//Create a variable for a user's age with a type annotation and assign a number to it.
+let presentage: number = 25;
+console.log(presentage);
+
+//Use a template string to display a product name and its price.
+let productname: string = "Coke";
+let productprice: number = 200;
+let productdetails: string = `Product name ${productname}, & price ${productprice}`;
+console.log(productdetails);
+
+// Declare a variable to store the number of pets you have with a number type annotation.
+let numberofpets: number = 4;
+console.log(numberofpets);
+
+//Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let todayisbirthday: boolean = false;
+console.log(todayisbirthday);
+
+//Create a template string that includes a user's first name, last name, and age with proper labels.
+let firstnam: string = "Muhammad Sajid";
+let secondnam: string = "Hameed";
+let ages: number = 25;
+let mydetails: string = `My name is ${firstnam} ${secondnam} and i am ${ages} years old.`;
+console.log(mydetails);
